4af1f02e7b238b26994f37e4de82b7a811f682c6
alexander jönsson <alexander.jxnsson@gmail.com>
2022-12-05 16:16:08 +0100

    first commit

---
+++ 2022/dag_ett/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+//std::ofstream myFile;
+std::ifstream myFile;
+std::string line;
+bool bFile = true, bCount = false;
+int bestElf = 0, kcal = 0, prevElf = 0;
+void fileOK();
+
+int main(){
+    while (bFile == true){
+            myFile.open("file.txt");
+        if (myFile.is_open()){
+            std::cout<<"File is OK\n";
+            bCount = true;
+            bFile = false;
+        }
+        else{
+            std::cout<<"File is not OK\n";
+            return 0;
+        }
+    }
+    
+    while (bCount == true){
+        while (std::getline(myFile, line))
+        {
+            kcal += std::stoi(line);
+            if (line == "\n"){
+                prevElf = kcal;
+                kcal = 0;
+                if (){
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+
+    return 0;   
+}
+
+void fileOK(){
+    
+}
