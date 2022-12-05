648e50df7fb19e80c2b19b8c059039ece93063bb
alexander jxnsson <alexander.jxnsson@gmail.com>
2022-12-05 18:55:26 +0100

    Day one DONE½

--- 2022/dag_ett/main.cpp
+++ 2022/dag_ett/main.cpp
@@ -5,20 +5,24 @@
 std::ifstream myFile;
 std::string line;
 bool bFile = true, bCount = false;
-int bestElf = 0, kcal = 0, prevElf = 0, bestElfnumber = 0;
+int bestElf = 0, kcal = 0, prevElf = 0;
 
 int main(){
     myFile.open("file.txt");
     
     while (std::getline(myFile, line)){
-        std::cout<<line<<std::endl;
-        kcal = std::stoi(line);
-        kcal += kcal;
-        prevElf = kcal;
+        if (line != ""){
+            kcal += std::stoi(line);
+            prevElf = kcal;
+        }
+        else if (line == ""){
+            kcal = 0;
+        }
+        
         if (prevElf > bestElf){
             bestElf = prevElf;
         }
-        kcal = 0;
+        
     }
     myFile.close();
     std::cout<<"The best elf carries: "<<bestElf;
