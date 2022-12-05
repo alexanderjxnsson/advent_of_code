a6d956566e6d401e3d06b0dd52e7580835b95cce
alexander jxnsson <alexander.jxnsson@gmail.com>
2022-12-05 16:48:15 +0100

    update day one

--- 2022/dag_ett/main.cpp
+++ 2022/dag_ett/main.cpp
@@ -1,46 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-//std::ofstream myFile;
+
 std::ifstream myFile;
 std::string line;
 bool bFile = true, bCount = false;
-int bestElf = 0, kcal = 0, prevElf = 0;
-void fileOK();
+int bestElf = 0, kcal = 0, prevElf = 0, bestElfnumber = 0;
 
 int main(){
-    while (bFile == true){
-            myFile.open("file.txt");
-        if (myFile.is_open()){
-            std::cout<<"File is OK\n";
-            bCount = true;
-            bFile = false;
-        }
-        else{
-            std::cout<<"File is not OK\n";
-            return 0;
-        }
-    }
+    myFile.open("file.txt");
     
-     while (bCount == true){
-        while (std::getline(myFile, line))
-        {
-            kcal += std::stoi(line);
-            if (line == "\n"){
-                prevElf = kcal;
-                if (prevElf > bestElf){
-                    bestElf = prevElf;
-                }
-                
-                kcal = 0;
+    while (std::getline(myFile, line)){
+        kcal += std::stoi(line);
+        if (line == "\n"){
+            prevElf = kcal;
+            if (prevElf > bestElf){
+                bestElf = prevElf;
             }
-            
+            kcal = 0;
         }
     }
-
+    myFile.close();
+    std::cout<<"The best elf carries: "<<bestElf;
     return 0;   
-}
-
-void fileOK(){
-    
 }
