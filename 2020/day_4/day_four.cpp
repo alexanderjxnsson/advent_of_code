a29047b829e2faf9be8ac766eef27c3f764e29c9
alexander jönsson <alexander.jxnsson@gmail.com>
2023-04-19 18:35:50 +0200

    2020 day four pt1 done

--- 2020/day_4/day_four.cpp
+++ 2020/day_4/day_four.cpp
@@ -1,44 +1,31 @@
-/* 
-    byr (Birth Year)
-    iyr (Issue Year)
-    eyr (Expiration Year)
-    hgt (Height)
-    hcl (Hair Color)
-    ecl (Eye Color)
-    pid (Passport ID)
-    cid (Country ID)
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
 
-int result = 0, mandatory = 0;
 std::ifstream file;
-std::string line, passport;
+int result = 0, mandatory = 0;
+std::string passportInfo;
 std::string mandatoryValues[7] = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
-
 
 
 int main(){
     file.open("input.txt");
 
-    while(std::getline(file, line)){
-        passport += line;
+    while(std::getline(file, passportInfo)){
 
-        if(line.size() == 0){
-            for (int i = 0; i < 7; i++){
-                if(passport.find(mandatoryValues[i]) != std::string::npos){
-                    mandatory++;
-                }
+        for (int i = 0; i < 7; i++){
+            if(passportInfo.find(mandatoryValues[i]) != std::string::npos){
+                mandatory++;
             }
-            if(mandatory == 7){
-                result++;
-            }
-            else{
-                mandatory = 0;
-                passport = "\0";
-            }
+        }
+
+        if(mandatory == 7){
+            result++;
+            passportInfo = "\0";
+        }
+
+        if(passportInfo.size() == 0){
+            mandatory = 0;
         }
     }
 
