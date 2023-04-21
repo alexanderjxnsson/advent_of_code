98dd82465b7ed87352728256a99bbedc19e2da2c
alexander jönsson <alexander.jxnsson@gmail.com>
2023-04-21 14:53:32 +0200

    2021 day three pt1 done, not cleaned up

--- 2021/day_3/day_three.cpp
+++ 2021/day_3/day_three.cpp
@@ -4,16 +4,46 @@
 #include <vector>
 
 std::ifstream file;
-std::string line;
-int i = 0, zeroCounter = 0, oneCounter = 0, gammaRateResult = 0, epsilonRateResult = 0, totalLines = 0;
-std::vector<std::string> vBinary, vGamma, vEpsilon;
 
 int main() {
-    file.open("test.txt", std::ifstream::in);
-    while(getline(file, line)){
-        vBinary.push_back(line);
-        totalLines++;
+    int countOne = 0, countZero = 0;
+    std::string binary;
+    std::vector<std::string> vBinary;
+    file.open("input.txt", std::ifstream::in);
+
+    while(getline(file, binary)){
+        vBinary.push_back(binary);
+    }
+    file.close();
+    std::string gamma;
+    std::string espilon;
+
+    for (int i = 0; i < vBinary[0].length(); i++){
+        for (int j = 0; j < vBinary.size(); j++){
+            if(vBinary[j].at(i) == '1')
+                countOne++;
+            else if(vBinary[j].at(i) == '0')
+                countZero++;
+        }
+        if(countOne > countZero){
+            countOne = 0;
+            countZero = 0;
+            gamma += '1';
+            espilon += '0';
+        }
+            
+        if(countZero > countOne){
+            countOne = 0;
+            countZero = 0;
+            gamma += '0';
+            espilon += '1';
+        }
     }
     
-    file.close();
+    int iGamma = std::stoi(gamma, 0, 2);
+    int iEpsilon = std::stoi(espilon, 0, 2);
+    int result = iGamma * iEpsilon;
+    std::cout << "result: " << result << std::endl;
+    
+    return 0;
 }
